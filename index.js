7e34ded4914a7a37b6857f6c2eb03de813d36532
manoj lodhi <manojlodhi0262@gmail.com>
2025-07-16 23:53:02 +0530

    complete mobile section and aad javascripts file

---
+++ index.js
@@ -0,0 +1,10 @@
+function d1(img){
+    
+    img.style.transform ="scale(1.2)"
+    img.style.transition="transform 0.7s"
+}
+function d2(img){
+   
+    img.style.transform ="scale(1)"
+    img.style.transition="transform 0.7s"
+}
