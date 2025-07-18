a5f37ad849ab652ec507cb6481c1b3396c8d9385
manoj lodhi <manojlodhi0262@gmail.com>
2025-07-18 14:25:19 +0530

    login ka dropdown banaya

--- index.js
+++ index.js
@@ -8,3 +8,63 @@ function d2(img){
     img.style.transform ="scale(1)"
     img.style.transition="transform 0.7s"
 }
+function k(){
+   let im = document.querySelector(".imgd")
+   im.src="https://rukminim2.flixcart.com/www/2140/1460/promos/26/09/2023/ed27f892-1bc6-462f-805b-953f5add4f6a.jpg?q=60"
+}
+function k1(){
+   let im = document.querySelector(".imgd")
+   im.src="https://rukminim2.flixcart.com/www/2140/1460/promos/15/09/2023/9b08f194-51e5-4e6a-a052-f5f91d75b8a0.jpg?q=60"
+}
+function run(){
+    let desing = `
+    <div class="drop">
+        <div class="drop1">
+           <a href="">
+             <p>New Customer?</p>
+            <span>
+                <p>Sign Up</p>
+            </span>
+           </a>
+        </div>
+        <hr>
+        <div>
+            <a href="">
+                <i class="fa-regular fa-circle-user"></i>
+            <span>My Profile</span>
+            </a>
+        </div>
+        <div>
+            <a href="">
+                <i class="fas fa-box"></i>
+                <span>Orders</span>
+            </a>
+        </div>
+         <div>
+            <a href="">
+                <i class="fas fa-heart"></i>
+                <span>Wishlist</span>
+            </a>
+        </div>
+        <div>
+            <a href="">
+                <i class="fas fa-gift"></i>
+                <span>Rewards</span>
+            </a>
+        </div>
+        <div>
+            <a href="">
+                <i class="fas fa-credit-card"></i>
+                <span>Gift Cards</span>
+            </a>
+        </div>
+    </div>
+    `
+    let output=document.querySelector("#output")
+    output.innerHTML = desing
+}
+function g(){
+    let desing =``
+    let output = document.querySelector("#output")
+    output.innerHTML =desing
+}
